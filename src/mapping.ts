d5dd88dc34faeaaf2e90d4e061a0608da55a177d
Alexander McElroy <admin@rhizomenetworking.com>
2021-12-08 23:56:32 -0500

    initial commit

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,83 +1,24 @@
 import { BigInt } from "@graphprotocol/graph-ts"
-import {
-  Contract,
-  AddSupportedChainId,
-  AddSwapToken,
-  Approval,
-  MigrateBridgeRole,
-  Mint,
-  RemoveSwapToken,
-  Swap,
-  Transfer,
-  Unwrap
-} from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+import { Transfer } from "../generated/Contract/Contract"
+import { Customer } from "../generated/schema"
 
-export function handleAddSupportedChainId(event: AddSupportedChainId): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleTransfer(event: Transfer): void {
+  let wallet = event.params.from
+  let register = event.params.to
+  let value = event.params.value
+  let customer_id = wallet.toHex() + "-" + register.toHex()
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  let customer = Customer.load(customer_id)
+  if (!customer) {
+    customer = new Customer(customer_id)
+    customer.sales = BigInt.fromI32(0)
+    customer.revenue = BigInt.fromI32(0)
+    customer.address = wallet
+    customer.register_address = register
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  customer.sales = customer.sales + BigInt.fromI32(1)
+  customer.revenue = customer.revenue + value
 
-  // Entity fields can be set based on event parameters
-  entity.chainId = event.params.chainId
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.allowance(...)
-  // - contract.approve(...)
-  // - contract.balanceOf(...)
-  // - contract.chainIds(...)
-  // - contract.decimals(...)
-  // - contract.decreaseAllowance(...)
-  // - contract.increaseAllowance(...)
-  // - contract.name(...)
-  // - contract.swapSupply(...)
-  // - contract.symbol(...)
-  // - contract.totalSupply(...)
-  // - contract.transfer(...)
-  // - contract.transferFrom(...)
+  customer.save()
 }
-
-export function handleAddSwapToken(event: AddSwapToken): void {}
-
-export function handleApproval(event: Approval): void {}
-
-export function handleMigrateBridgeRole(event: MigrateBridgeRole): void {}
-
-export function handleMint(event: Mint): void {}
-
-export function handleRemoveSwapToken(event: RemoveSwapToken): void {}
-
-export function handleSwap(event: Swap): void {}
-
-export function handleTransfer(event: Transfer): void {}
-
-export function handleUnwrap(event: Unwrap): void {}
